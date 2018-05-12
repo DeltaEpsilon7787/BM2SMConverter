12b5ad98eb796c71017acda2a0f5e1111ad5c9a9
DeltaEpsilon7787 <editorrus2@gmail.com>
2018-05-13 02:05:49 +0600

    Add key singletons

---
+++ rhythm_interface/singletons.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class Key(Enum):
+    KEY_1 = object()
+    KEY_2 = object()
+    KEY_3 = object()
+    KEY_4 = object()
+    KEY_5 = object()
+    KEY_6 = object()
+    KEY_7 = object()
+    KEY_8 = object()
+    KEY_9 = object()
+    KEY_10 = object()
+    KEY_11 = object()
+    KEY_12 = object()
+    KEY_13 = object()
+    KEY_14 = object()
+    KEY_15 = object()
+    KEY_16 = object()
+    KEY_17 = object()
+    KEY_18 = object()
+    KEY_19 = object()
+    KEY_20 = object()
