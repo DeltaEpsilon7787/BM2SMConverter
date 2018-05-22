2f828eb049e5dfbdb1b964e882d51b887dbf33cb
DeltaEpsilon7787 <editorrus2@gmail.com>
2018-05-22 06:43:44 +0600

    Traits are even more magical and rhythm language is even more abstract

--- rhythm_interface/known_objects.py
+++ rhythm_interface/known_objects.py
@@ -1,55 +1,59 @@
-from rhythm_interface.traits import Declarative, Keyed, RhythmTime, Structural
+from rhythm_interface.traits import TraitMeta
 
 
-# Notes
-class TapObject(Keyed, RhythmTime):
-    abstract_position_trait = None
+# Most VSRG should never utilize this directly anyway
+class RhythmObject(object, metaclass=TraitMeta):
+    pass
 
 
-class LNObject(Keyed, RhythmTime):
-    start_position_trait = None
-    end_position_trait = None
+# Abstract stuff
+class RhythmRowPoint(RhythmObject):
+    abstract_row_point_position_trait = None
 
 
-# Headers
-class ChartTitle(Declarative):
-    title_trait = None
+class RhythmColumnPoint(RhythmObject):
+    abstract_column_point_position_trait = None
 
 
-class ChartArtist(Declarative):
-    artist_trait = None
+class RhythmRowInterval(RhythmObject):
+    abstract_row_interval_start_position_trait = None
+    abstract_row_interval_end_position_trait = None
 
 
-class ChartSubtitle(Declarative):
-    subtitle_trait = None
+class RhythmColumnInterval(RhythmObject):
+    abstract_column_interval_start_position_trait = None
+    abstract_column_interval_end_position_trait = None
 
 
-class ChartGenre(Declarative):
-    genre_trait = None
+class RhythmState(RhythmObject):
+    abstract_state_key_trait = None
+    abstract_state_value_trait = None
 
 
-class ChartCredit(Declarative):
-    credit_trait = None
+class RhythmCommand(RhythmObject):
+    abstract_command_trait = None
 
 
-class ChartDifficulty(Declarative):
-    abstract_difficulty_trait = None
+# Basic meta data stuff
+class Banner(RhythmState):
+    chart_banner_trait = None
 
 
-class ChartBGA(Declarative):
-    background_image_trait = None
+class Title(RhythmState):
+    chart_title_trait = None
 
 
-class ChartBGM(Declarative):
-    background_audio_trait = None
+class Subtitle(RhythmState):
+    chart_subtitle_trait = None
 
 
-# Timing sections
-class BPMChange(Structural):
-    start_position_trait = None
-    bpm_trait = None
+class Artist(RhythmState):
+    chart_artist_trait = None
 
 
-class Stop(Structural):
-    stop_position_trait = None
-    abstract_stop_duration_trait = None
+class Maker(RhythmState):
+    chart_maker_trait = None
+
+
+class Genre(RhythmState):
+    chart_genre_trait = None
--- rhythm_interface/traits.py
+++ rhythm_interface/traits.py
@@ -1,4 +1,14 @@
 class TraitError(Exception):
+    def __init__(self, trait_name=None, source_name=None):
+        self.trait_name = trait_name
+        self.source_name = source_name
+
+
+class UndefinedTraitError(TraitError):
+    pass
+
+
+class ImmutableTraitError(TraitError):
     pass
 
 
@@ -6,9 +16,10 @@ class TraitMeta(type):
     """A metaclass used to implement traits.
 
     Search for attributes ending with _trait and make them one-time set property.
+    The name of the property is the same as the trait name, but without _trait.
     Once the value is set, it can be read, but not modified.
-    Attempting to read the attribute before setting it will raise TraitError
-    Attempting to set the attribute after setting it will raise TraitError
+    Attempting to read the attribute before setting it will raise UndefinedTraitError
+    Attempting to set the attribute after setting it will raise ImmutableTraitError
     """
 
     def __new__(mcs, name, bases, attributes):
@@ -18,13 +29,15 @@ class TraitMeta(type):
             def read_value(self):
                 nonlocal has_been_set
                 if not has_been_set:
-                    raise TraitError('Trait {} must be defined for {}'.format(property_name, name))
+                    raise UndefinedTraitError(trait_name=property_name,
+                                              source_name=name)
                 return getattr(self, '_' + property_name)
 
             def set_value(self, value):
                 nonlocal has_been_set
                 if has_been_set:
-                    raise TraitError('Trait {} has already been defined for {}'.format(property_name, name))
+                    raise ImmutableTraitError(trait_name=property_name,
+                                              source_name=name)
                 setattr(self, '_' + property_name, value)
                 has_been_set = True
 
@@ -34,54 +47,6 @@ class TraitMeta(type):
         for attr_name, attr_value in attributes.items():
             if not attr_name.endswith('_trait'):
                 continue
-            changed_attributes[attr_name] = make_trait_property(attr_name)
+            changed_attributes[attr_name[:-6]] = make_trait_property(attr_name)
             changed_attributes['_' + attr_name] = attr_value
         return super(TraitMeta, mcs).__new__(mcs, name, bases, changed_attributes)
-
-
-class RhythmObject(object, metaclass=TraitMeta):
-    def convert_to(self, other_type):
-        """Attempt to convert this object to another object
-
-        Arguments:
-            other_type (RhythmObject):
-                Which type to try to convert to?
-
-        Returns:
-            other_type:
-                Converted object
-
-        Raises:
-            TypeError:
-                If this object cannot be converted to `other_type`
-            """
-        raise TypeError
-
-
-class RhythmTime(RhythmObject):
-    abstract_time_trait = None
-
-
-class AbsoluteTime(RhythmTime):
-    abstract_absolute_time_trait = None
-
-
-class RelativeTime(RhythmTime):
-    abstract_relative_time_trait = None
-
-
-class Keyed(RhythmObject):
-    abstract_key_trait = None
-
-
-class Fundamental(RhythmObject):
-    abstract_fundamental_value_trait = None
-
-
-class Declarative(RhythmObject):
-    abstract_value_trait = None
-
-
-class Structural(RhythmTime):
-    abstract_time_trait = None
-    abstract_value_trait = None
