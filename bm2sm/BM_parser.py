698d97b93484eb183999f27ff8865cc25be8f4d5
Delta <editorrus2@gmail.com>
2021-06-02 05:58:56 +0500

    Fix UndecidableAudioFile when the directory contains square brackets... but for real

--- bm2sm/BM_parser.py
+++ bm2sm/BM_parser.py
@@ -1,6 +1,6 @@
 import operator
 from fractions import Fraction
-from glob import glob
+from glob import glob, escape
 from os import path
 from re import findall, match
 from shutil import copy2
@@ -149,7 +149,7 @@ class BMChartParser(object):
     def _define_wav(self, value, wav_id):
         header = path.dirname(self.BM_file_path)
         filename = path.splitext(path.basename(value))[0]
-        file_path = path.join(header, filename)
+        file_path = escape(path.join(header, filename))
         candidates = glob(file_path + '.*')
 
         if len(candidates) != 1:
